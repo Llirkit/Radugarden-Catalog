9a0357ff59dfb2d7b2c4989f53831619b19a1273
Llirkit <loooool812@gmail.com>
2023-08-29 15:59:28 +0700

    Added quasar

--- src/main.js
+++ src/main.js
@@ -2,14 +2,36 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import components from '@/components/UI';
 import router from '@/router/router.js'
+import { Quasar } from 'quasar'
 
-const app = createApp(App)
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const myApp = createApp(App)
 
 components.forEach(component => {
-    app.component(component.name, component)
+    myApp.component(component.name, component)
 });
 
-app
-    .use(router)
-    .mount('#app')
+myApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+})
+.use(router)
 
+// Assumes you have a <div id="app"></div> in your index.html
+myApp.mount('#app')
