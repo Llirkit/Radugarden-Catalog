d5fd3732d8540971574b1f601cce92d8f4c485bb
Llirkit <loooool812@gmail.com>
2023-10-07 15:06:48 +0700

    t4

--- src/pages/ItemPage.vue
+++ src/pages/ItemPage.vue
@@ -7,7 +7,7 @@ import usePost from '@/hooks/usePost'
 const route = useRoute()
 const item = ref()
 const picture = ref()
-const test3 = ref()
+const test4 = ref()
 
 const useNotif = () => {
   Notify.create('Messege')
